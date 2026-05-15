0fc532ca53103d983b6e9f98746d44d4c1fdfd99
FroxenMC <hgggopfunnu@gmail.com>
2026-05-15 18:44:12 +0530

    Update image.js

--- commands/image.js
+++ commands/image.js
@@ -3,39 +3,97 @@ const axios = require('axios');
 module.exports = {
   name: 'image',
 
-  async execute(message, args) {
+  async execute(message,args){
 
-    try {
+    try{
 
-      const prompt = args.join(' ');
+      const attachments=
+      [...message.attachments.values()];
 
-      if (!prompt)
+      const userPrompt=
+      args.join(" ");
+
+      if(attachments.length<2){
+
         return message.reply(
-          'Use: RL!image <prompt>'
+        "📸 Upload BOTH Lampy + Rose skins"
         );
 
-      const msg =
+      }
+
+      const loading=
       await message.reply(
-        '🎨 Twohearts drawing...'
+      "🎨 Twohearts making Lampy + Rose..."
       );
 
-      const url =
-`https://image.pollinations.ai/prompt/${encodeURIComponent(prompt)}?width=1024&height=1024`;
+      const megaPrompt=
+encodeURIComponent(`
 
-      await message.channel.send({
-        content:
-`💕 ${prompt}`,
-        files:[url]
+EXACTLY TWO minecraft characters.
+
+NO random Steve.
+NO extra people.
+NO different outfits.
+
+Boy:
+white hair
+grey hoodie
+dark aesthetic
+
+Girl:
+long blonde hair
+pink outfit
+flower accessory
+
+Cute romantic couple
+
+Holding hands
+
+Minecraft sunset wallpaper
+
+Shaders
+
+Cinematic lighting
+
+${userPrompt}
+
+`);
+
+      const url=
+`https://image.pollinations.ai/prompt/${megaPrompt}`;
+
+      const response=
+      await axios.get(
+      url,
+      {
+      responseType:'arraybuffer'
       });
 
-      await msg.delete();
+      const buffer=
+      Buffer.from(response.data);
 
-    } catch(err){
+      await message.channel.send({
+
+      content:
+      "💕 Lampy + Rose",
+
+      files:[
+      {
+      attachment:buffer,
+      name:"twohearts.png"
+      }
+      ]
+
+      });
+
+      loading.delete();
+
+    }catch(err){
 
       console.log(err);
 
       message.reply(
-        '❌ image failed'
+      "❌ generation failed"
       );
 
     }
