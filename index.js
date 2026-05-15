558dfc3fc2b11f9f597af58a15ef0099571c9e2c
FroxenMC <hgggopfunnu@gmail.com>
2026-05-15 16:43:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const fs = require('fs');
 
 const client = new Client({
   intents: [
@@ -9,45 +8,19 @@ const client = new Client({
   ]
 });
 
-const prefix = 'RL!';
-
-const commands = new Map();
-
-const commandFiles = fs
-  .readdirSync('./commands')
-  .filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  commands.set(command.name, command);
-}
-
 client.once('clientReady', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async (message) => {
   if (message.author.bot) return;
 
-  if (!message.content.startsWith(prefix)) return;
+  const msg = message.content;
 
-  const args = message.content
-    .slice(prefix.length)
-    .trim()
-    .split(/ +/);
+  if (!msg) return;
 
-  const commandName = args.shift()?.toLowerCase();
-
-  const command = commands.get(commandName);
-
-  if (!command) return;
-
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply('Command error :(');
-  }
+  // Temporary fake AI response
+  message.reply(`You said: ${msg}`);
 });
 
 client.login(process.env.TOKEN);
