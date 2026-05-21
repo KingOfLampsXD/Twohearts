aeb7c19be68b0fe2f58559e037275f3f7ce22b71
FroxenMC <hgggopfunnu@gmail.com>
2026-05-21 16:57:35 +0530

    Update relationship.js

--- commands/relationship.js
+++ commands/relationship.js
@@ -5,9 +5,11 @@ module.exports={
 name:"confess",
 
 aliases:[
+
 "3am",
 "heartbeatrace",
 "answer"
+
 ],
 
 async execute(message,args){
@@ -15,8 +17,11 @@ async execute(message,args){
 const cmd=
 
 message.content
+
 .toLowerCase()
+
 .replace(/^rl!/,"")
+
 .split(/ +/)[0];
 
 
@@ -29,36 +34,43 @@ await message.reply(
 
 `💞 Confession Night
 
-You BOTH have 30 sec
+You BOTH have 30 sec 😭
 
-Reply:
+Reply using:
 
 RL!answer your text
 
-Secret answers 😭`
+Nobody sees answers.`
 
 );
 
 
 setTimeout(()=>{
 
-const users=[...answers.keys()];
+
+const users=
+
+[...answers.keys()];
 
 
 if(users.length<2){
 
 message.channel.send(
 
-"😭 confession failed not enough answers"
+"😭 confession failed\nnot enough answers"
 
 );
+
+answers.clear();
 
 return;
 
 }
 
 
+
 const values=
+
 [...answers.entries()];
 
 
@@ -66,19 +78,23 @@ message.channel.send(
 
 `💌 Hidden Answers Revealed
 
-${values[0][0]}:
+🤍 ${values[0][0]}:
 
 ${values[0][1]}
 
+
 💞
 
-${values[1][0]}:
+
+🤍 ${values[1][0]}:
 
 ${values[1][1]}
+
 
 +50 Love Points 😭`
 
 );
+
 
 answers.clear();
 
@@ -91,9 +107,12 @@ return;
 
 
 
+
+
 if(cmd==="answer"){
 
 const text=
+
 args.join(" ");
 
 
@@ -117,13 +136,141 @@ text
 );
 
 
+
+// delete message instantly
+
+message.delete()
+
+.catch(()=>{});
+
+
+
+// DM confirmation
+
+message.author.send(
+
+"🤫 confession saved"
+
+)
+
+.catch(()=>{});
+
+
+return;
+
+}
+
+
+
+
+
+if(
+cmd==="3am"
+){
+
+const questions=[
+
+"if you were here rn i would ____",
+
+"one thing i secretly miss is ____",
+
+"stay 5 more mins because ____",
+
+"distance disappears and suddenly ____",
+
+"late night thought: ____"
+
+];
+
+
 return message.reply(
 
-"🤫 answer saved"
+`🌙 3AM Mode 😭
+
+${
+questions[
+Math.floor(
+Math.random()
+*
+questions.length
+)
+]
+}`
 
 );
 
 }
 
+
+
+
+
+if(
+cmd==="heartbeatrace"
+){
+
+const msg=
+
+await message.reply(
+
+`❤️ HEARTBEAT RACE
+
+Spam ❤️
+
+15 sec 😭`
+
+);
+
+
+await msg.react(
+"❤️"
+);
+
+
+setTimeout(
+
+async()=>{
+
+
+const fetched=
+
+await msg.fetch();
+
+
+const reaction=
+
+fetched.reactions.cache.get(
+"❤️"
+);
+
+
+const count=
+
+reaction?.count-1
+||0;
+
+
+message.channel.send(
+
+`😭 HEART ATTACK ENERGY
+
+❤️:
+${count}`
+
+);
+
+},
+
+15000
+
+);
+
+
+return;
+
 }
+
+
+}
+
 };
